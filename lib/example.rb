bcb02b4e8473669d0f9b6fde83381664b502425b
Tyler Lasseter <tslasseter@gmail.com>
2020-09-29 20:43:42 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,34 +1,6 @@
-# Use this space to code along with the readme
-
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-
-def say_hello_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
-say_hello_ten_times
-
 def greeting
   puts "Hello World!"
 end
+greeting
 
 greeting
